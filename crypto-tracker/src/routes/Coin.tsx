d519225a86e86d62335410301386ee8d9a19772b
ksw <ksw@codeblock.co.kr>
2023-04-13 17:36:51 +0900

    04131756

--- crypto-tracker/src/routes/Coin.tsx
+++ crypto-tracker/src/routes/Coin.tsx
@@ -1,6 +1,10 @@
-import { useState } from "react";
-import { useLocation, useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Link, Route, Switch, useLocation, useParams, useRouteMatch } from "react-router-dom";
 import styled from "styled-components";
+import Price from "./Price";
+import Chart from "./Chart";
+import { useQuery } from "react-query";
+import { fetchCoinInfo, fetchCoinTickers } from "../api";
 
 const Container = styled.div`
     padding: 0px 20px;
@@ -24,22 +28,175 @@ const Loader = styled.span`
     text-align: center;
     display: block;
 `
+
+const Overview = styled.div`
+  display: flex;
+  justify-content: space-between;
+  background-color: rgba(0, 0, 0, 0.5);
+  padding: 10px 20px;
+  border-radius: 10px;
+`;
+
+const OverviewItem = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  span:first-child {
+    font-size: 10px;
+    font-weight: 400;
+    text-transform: uppercase;
+    margin-bottom: 5px;
+  }
+`;
+
+const Description = styled.p`
+  margin: 20px 0px;
+`;
+
+const Tabs = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  margin: 25px 0px;
+  gap: 10px;
+`;
+
+const Tab = styled.span<{ isActive: boolean }>`
+  text-align: center;
+  text-transform: uppercase;
+  font-size: 12px;
+  font-weight: 400;
+  background-color: rgba(0, 0, 0, 0.5);
+  padding: 7px 0px;
+  border-radius: 10px;
+  color: ${props => 
+    props.isActive ? props.theme.accentColor : props.theme.textColor }
+    !important;
+  a {
+    display: block;
+  }
+`;
+
 interface RouteState {
     name: string;
 }
 
+interface IInfoData {
+    id: string;
+    name: string;
+    symbol: string;
+    rank: number;
+    is_new: boolean;
+    is_active: boolean;
+    type: string;
+    logo: string;
+    description: string;
+    message: string;
+    open_source: boolean;
+    started_at: string;
+    development_status: string;
+    hardware_wallet: boolean;
+    proof_type: string;
+    org_structure: string;
+    hash_algorithm: string;
+    first_data_at: string;
+    last_data_at: string;
+}
+
+interface IPriceData {
+    id: string;
+    name: string;
+    symbol: string;
+    rank: number;
+    circulating_supply: number;
+    total_supply: number;
+    max_supply: number;
+    beta_value: number;
+    first_data_at: string;
+    last_updated: string;
+    qutoes: {
+        USD: {
+            price: number;
+            volume_24h: number;
+            volume_24h_change_24h: number;
+            market_cap: number;
+            market_cap_change_24h: number;
+            percent_change_15m: number;
+            percent_change_30m: number;
+            percent_change_1h: number;
+            percent_change_6h: number;
+            percent_change_12h: number;
+            percent_change_24h: number;
+            percent_change_7d: number;
+            percent_change_30d: number;
+            percent_change_1y: number;
+            ath_price: number;
+            ath_date: Date;
+            percent_from_price_ath: number;
+        }
+    }
+}
+
 function Coin() {
-    const [loading, setLoading] = useState(true);
     const { coinId } = useParams<{ coinId: string }>();
     const { state } = useLocation<RouteState>();
-    console.log(state.name);
+    const priceMatch = useRouteMatch("/:coinId/price");
+    const chartMatch = useRouteMatch("/:coinId/chart");
+    const { isLoading: infoLoading, data: infoData } = useQuery<IInfoData>(["info", coinId], () => fetchCoinInfo(coinId));
+    const { isLoading: tickersLoading, data: tickersData } = useQuery<IPriceData>(["tickers", coinId], () => fetchCoinTickers(coinId));
+    const loading = infoLoading || tickersLoading;
 
     return (
         <Container>
             <Header>
-                <Title>{ state?.name || "Loading" }</Title>
+                {state?.name ? state.name : loading ? "Loading..." : infoData?.name}
             </Header>
-            {loading ? <Loader>"Loading..."</Loader> : null}
+            {loading ? <Loader>"Loading..."</Loader> : (
+                <>
+                    <Overview>
+                        <OverviewItem>
+                            <span>Rank:</span>
+                            <span>{infoData?.rank}</span>
+                        </OverviewItem>
+                        <OverviewItem>
+                            <span>Symbol:</span>
+                            <span>${infoData?.symbol}</span>
+                        </OverviewItem>
+                        <OverviewItem>
+                            <span>Open Source:</span>
+                            <span>{infoData?.open_source ? "Yes" : "No"}</span>
+                        </OverviewItem>
+                    </Overview>
+                    <Description>{infoData?.description}</Description>
+                    <Overview>
+                        <OverviewItem>
+                            <span>Total Suply:</span>
+                            <span>{tickersData?.total_supply}</span>
+                        </OverviewItem>
+                        <OverviewItem>
+                            <span>Max Supply:</span>
+                            <span>{tickersData?.max_supply}</span>
+                        </OverviewItem>
+                    </Overview>
+
+                    <Tabs>
+                        <Tab isActive={chartMatch !== null}>
+                            <Link to={`/${coinId}/chart`}>Chart</Link>
+                        </Tab>
+                        <Tab isActive={priceMatch !== null}>
+                            <Link to={`/${coinId}/price`}>Price</Link>
+                        </Tab>
+                    </Tabs>
+
+                    <Switch>
+                        <Route path={`/${coinId}/price`}>
+                            <Price />
+                        </Route>
+                        <Route path={`/${coinId}/chart`}>
+                            <Chart />
+                        </Route>
+                    </Switch>
+                </>
+            )}
         </Container>
     )
 }
